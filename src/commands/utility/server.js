76468232d445aff0f6a0b9fe7dcf55deaf4748df
joshua <joshuaslui0203@gmail.com>
2021-09-19 21:15:53 +0200

    Ping server

--- src/commands/utility/server.js
+++ src/commands/utility/server.js
@@ -1,4 +1,6 @@
 const {ping} = require('minecraft-protocol');
+const Discord = require("discord.js");
+const { color } = require('../../util')
 
 module.exports = {
     name: 'status',
@@ -9,7 +11,38 @@ module.exports = {
 
     cooldown: 60,
 
-    async execute(message, args, client) {
-        console.log(await ping({host: 'MinecraftGolden.aternos.me'}));
+async execute(message, args, client) {
+        function removeColorsFromString (text) { // Removing minecraft colors from strings, because console can`t read it and it will look crazy.
+            return text.replace(/§./g, '')
+        }
+
+        const test = await ping({host: 'MinecraftGolden.aternos.me'});
+        console.log(test);
+
+        if(test.version.name === '§4● Offline') {
+            const embed = new Discord.MessageEmbed()
+                .setTitle(`MinecraftGolden.aternos.me`)
+                .addFields(
+                    /** @type {any} */ {name: '__**Status**__', value: 'Offline', inline: true},
+                    /** @type {any} */ {name: '__**Players**__', value: JSON.stringify(test.players.max) + ` playerslots`, inline: true},
+                    /** @type {any} */ {name: '__**Latency**__', value: JSON.stringify(test.latency), inline: true}
+                )
+                .setColor(color.red)
+                .setFooter(`Command executed by ${message.author.tag}`)
+                .setTimestamp()
+            await message.reply( { embeds: [embed] } )
+        } else {
+            const embed = new Discord.MessageEmbed()
+                .setTitle(`MinecraftGolden.aternos.me`)
+                .addFields(
+                    /** @type {any} */ {name: '__**Status**__', value: 'Online', inline: true},
+                    /** @type {any} */ {name: '__**Players**__', value: JSON.stringify(test.players.online) + `/` + JSON.stringify(test.players.max), inline: true},
+                    /** @type {any} */ {name: '__**Latency**__', value: JSON.stringify(test.latency), inline: true}
+                )
+                .setColor(color.green)
+                .setFooter(`Command executed by ${message.author.tag}`)
+                .setTimestamp()
+            await message.reply({embeds: [embed]});
+        }
     },
 };
