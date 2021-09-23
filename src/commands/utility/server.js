02274eed530ec287617545e57f4255c2961c89ad
joshua <joshuaslui0203@gmail.com>
2021-09-23 17:47:23 +0200

    Fix usage

--- src/commands/utility/server.js
+++ src/commands/utility/server.js
@@ -9,7 +9,7 @@ module.exports = {
 
     aliases: ['s', 'status'],
 
-    usage: '<server IP (without .aternos.me)',
+    usage: '<Aternos server IP>',
 
     description: 'Get the server status',
 
