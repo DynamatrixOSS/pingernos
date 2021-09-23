21e62e7a56c9694f479775f9eeafe03d12397937
joshua <joshuaslui0203@gmail.com>
2021-09-23 18:43:43 +0200

    Privacy command

---
+++ src/commands/utility/privacy.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const util = require("../../util")
+
+module.exports = {
+    name: 'privacy',
+
+    aliases: ['policy', 'pp'],
+
+    description: 'See our privacy policy.',
+
+
+    async execute(message, args, client) {
+        const embed = new Discord.MessageEmbed()
+            .setDescription(`${client.user.username} Does not save any data. For any concerns, [join our support server](${util.links.support})`)
+            .setColor(util.color.blue)
+
+        await message.reply({ embeds: [embed] });
+
+    },
+};
