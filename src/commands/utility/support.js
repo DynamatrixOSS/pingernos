ccdd31cebc4a550d0690e565fad664273a80a0b6
joshua <joshuaslui0203@gmail.com>
2021-09-23 18:40:12 +0200

    Support command

---
+++ src/commands/utility/support.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const util = require("../../util")
+
+module.exports = {
+    name: 'support',
+
+    description: 'Get support here',
+
+
+    async execute(message, args, client) {
+        const embed = new Discord.MessageEmbed()
+            .setDescription(`[join our support server](${util.links.support}) or contact **modraxisorg@gmail.com**.`)
+            .setColor(util.color.blue)
+
+        await message.reply({ embeds: [embed] });
+
+    },
+};
