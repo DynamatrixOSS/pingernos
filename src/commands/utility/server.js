7cde6066bdd4dbe45f0c88d6d61aea0b98f0f258
joshua <joshuaslui0203@gmail.com>
2021-09-23 17:06:53 +0200

    check status of any aternos server Thanks Julian for the regex help :)

--- src/commands/utility/server.js
+++ src/commands/utility/server.js
@@ -5,7 +5,11 @@ const { color } = require('../../util')
 module.exports = {
     name: 'status',
 
+    args: true,
+
     aliases: ['s', 'status'],
+
+    usage: '<server IP (without .aternos.me)',
 
     description: 'Get the server status',
 
@@ -16,11 +20,14 @@ async execute(message, args, client) {
             return text.replace(/§./g, '')
         }
 
-        const test = await ping({host: 'MinecraftGolden.aternos.me'});
+        console.log(args[0])
+        const ip = args[0].match(/^(\w+)(?:\.aternos\.me)?$/i)[1];
+
+        const test = await ping({host: `${ip}.aternos.me`});
 
         if(test.version.name === '§4● Offline') {
             const embed = new Discord.MessageEmbed()
-                .setTitle(`MinecraftGolden.aternos.me`)
+                .setTitle(`${ip}.aternos.me`)
                 .addFields(
                     /** @type {any} */ {name: '__**Status**__', value: 'Offline', inline: true},
                     /** @type {any} */ {name: '__**Players**__', value: 'unknown', inline: true},
@@ -32,7 +39,7 @@ async execute(message, args, client) {
             await message.reply( { embeds: [embed] } )
         } else {
             const embed = new Discord.MessageEmbed()
-                .setTitle(`MinecraftGolden.aternos.me`)
+                .setTitle(`${ip}.aternos.me`)
                 .addFields(
                     /** @type {any} */ {name: '__**Status**__', value: 'Online', inline: true},
                     /** @type {any} */ {name: '__**Players**__', value: JSON.stringify(test.players.online) + `/` + JSON.stringify(test.players.max), inline: true},
