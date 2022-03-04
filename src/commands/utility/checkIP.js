25e6d6f268a9a5e0954dfbb2d9ba8b460bf0e7bc
ToothyDev <chosenfate@gmx.de>
2022-03-04 21:46:53 +0100

    minor indentation fix

--- src/commands/utility/checkIP.js
+++ src/commands/utility/checkIP.js
@@ -10,13 +10,13 @@ module.exports = {
   
   async execute(message, args, client) {
     function removeColorsFromString(text) {
-        // Removing minecraft colors from strings, because console can`t read it and it will look crazy.
-        return text.replace(/§./g, "");
+      // Removing minecraft colors from strings, because console can`t read it and it will look crazy.
+      return text.replace(/§./g, "");
     }
     let ip = args[0].match(/^(\w+)(?:\.aternos\.me)?$/i);
     if (!ip) {
-        await message.reply(`\`${args}\` is not a valid Aternos server IP or name.`);
-        return;
+      await message.reply(`\`${args}\` is not a valid Aternos server IP or name.`);
+      return;
     }
     ip = ip[1];
 
