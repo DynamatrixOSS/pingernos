2cb7bbf794502bd35887fd6924f53e087cb2eebc
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2022-08-24 18:04:29 +0200

    Update info.js

--- src/commands/utility/info.js
+++ src/commands/utility/info.js
@@ -11,7 +11,7 @@ module.exports = {
     async execute(message, args, client) {
         const embed = new Discord.MessageEmbed()
             .setDescription(
-                `${client.user.username} is not affiliated with Aternos GmbH, but is a bot developed by [PsychOps Studios](https://psychops.tk) with the purpose of offering Aternos users a public alternative to the private Aternos bot.\n\n[[Invite]](${util.links.invite}) [[Support]](${util.links.support}) [[Github]](https://github.com/PsychOps/serverpinger) [[Aternos]](https://aternos.org/)`
+                `${client.user.username} is not affiliated with Aternos GmbH, but is a bot developed by [PsychOps Studios](https://psychops.eu) with the purpose of offering Aternos users a public alternative to the private Aternos bot.\n\n[[Invite]](${util.links.invite}) [[Support]](${util.links.support}) [[Github]](https://github.com/PsychOps/serverpinger) [[Aternos]](https://aternos.org/)`
             )
             .setColor(util.color.blue);
 
