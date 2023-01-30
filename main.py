00c0d7e69cc0ccd0845666f825d73cd63e4d3de3
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2023-01-30 17:34:50 -0500

    remove comments, that'll probably be fine tbh

--- main.py
+++ main.py
@@ -3,22 +3,16 @@ from discord.ext.bridge import Bot
 from utils import Utils
 data = Utils.get_data()
 intents = Intents(guilds=True, guild_messages=True)
-#intents.message_content = True #Uncomment this if you use prefixed command that are not mentions (e.g. !help)
+#intents.message_content = True #Uncomment this if you use prefixed command that are not mentions
 bot = Bot(intents=intents, command_prefix=data['Prefix'], status=Status.dnd, activity=Activity(type=ActivityType.playing, name="Booting..."))
-#bot.remove_command('help') #Removes the default help command
 bot.load_extensions("cogs") #Loads all cogs in the cogs folder
-BOOTED = False
 @bot.event
 async def on_connect():
     print('Connected to Discord!')
-
 @bot.event
 async def on_ready():
-    global BOOTED
-    if BOOTED is False:
-        await bot.change_presence(activity=Activity(type=ActivityType.watching, name="you (prefix: @mention)"), status=Status.dnd)
-        BOOTED = True #This is to prevent the bot from changing its presence every time it reconnects, even though this is in general a bad coding practice to redefine an outside variable in a function.
     print(f'Logged in as {bot.user}')
     print('------')
+    await bot.change_presence(activity=Activity(type=ActivityType.watching, name="you (prefix: @mention)"), status=Status.dnd)
 
 bot.run(data['Token'])
