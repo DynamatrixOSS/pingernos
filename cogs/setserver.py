0296265bdd89c8abb9a35147b7000fba1e837a30
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2023-01-31 15:30:45 -0500

    Disable that for now

--- cogs/setserver.py
+++ cogs/setserver.py
@@ -1,13 +1,13 @@
-from discord.ext import commands, bridge
+from discord.ext import commands#, bridge
 class SetServer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @bridge.bridge_command(aliases=["set"], description="Set the default server to use if no argument is provided in the status command.")
-    async def setserver(self, ctx, server = None):
-        if server is None:
-            return await ctx.respond("Please provide a server IP.")
-        await ctx.defer()
+    #@bridge.bridge_command(aliases=["set"], description="Set the default server to use if no argument is provided in the status command.")
+    #async def setserver(self, ctx, server = None):
+    #    if server is None:
+    #        return await ctx.respond("Please provide a server IP.")
+    #    await ctx.defer()
         #This is up to Miataboy to implement
 
 def setup(bot):
