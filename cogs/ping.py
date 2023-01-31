f5da351f8e4835b4b76db766f17d33016a8ecb7c
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2023-01-31 15:06:18 -0500

    remove defer, change timeout

--- cogs/ping.py
+++ cogs/ping.py
@@ -7,9 +7,9 @@ class Ping(commands.Cog):
 
     @bridge.bridge_command(aliases=["latency", "pong"], description="Get the bot and websocket latency")
     async def ping(self, ctx):
-        await ctx.defer()
+        #await ctx.defer()
         try:
-            stat = await wait_for(Utils.get_server_status("example.aternos.me"), timeout=3)
+            stat = await wait_for(Utils.get_server_status("example.aternos.me"), timeout=2)
         except TimeoutError:
             return await ctx.respond(f"Uh oh! The protocol took too long to respond! This will likely fix itself.\nAPI Latency is {round(self.bot.latency*1000, 2)}ms")
         await ctx.respond(content=f"API Latency is {round(self.bot.latency*1000, 2)}ms\nProtocol: {round(stat.latency)}ms")
