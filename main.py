f80f246400003c024b64eeba1c30d4871920f339
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2023-01-31 15:02:23 -0500

    Bridge commands are gonna kill me

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ intents = Intents(guilds=True, guild_messages=True)
 #intents.message_content = True #Uncomment this if you use prefixed command that are not mentions
 bot = Bot(intents=intents, command_prefix=data['Prefix'], status=Status.dnd, activity=Activity(type=ActivityType.playing, name="you (prefix: @mention)"), debug_guilds=[773950337303314518])
 bot.load_extensions("cogs") #Loads all cogs in the cogs folder
+BOOTED = False
 @bot.listen()
 async def on_connect():
     print('Connected to Discord!')
 
 @bot.listen()
 async def on_ready():
-    print(f'Logged in as {bot.user}')
-    print('------')
-    #await bot.sync_commands()
+    global BOOTED
+    if not BOOTED:
+        await bot.sync_commands()
+        print(f'Logged in as {bot.user}')
+        print('------')
+        BOOTED = True
+    if BOOTED:
+        print ("Reconnect(?)")
 
 bot.run(data['Token'])
