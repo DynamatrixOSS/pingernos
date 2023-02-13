7922ca565ce4f678e17bfecf7b6127f52d4a05f6
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2023-02-13 16:06:44 -0500

    See if pylint gets fixed by that

--- cogs/info.py
+++ cogs/info.py
@@ -1,6 +1,7 @@
 from discord.ext import commands, bridge
 from discord import Embed
 from utils import Utils
+from asyncio import wait_for, TimeoutError
 
 class Info(commands.Cog):
     def __init__(self, bot):
@@ -9,11 +10,19 @@ class Info(commands.Cog):
     @bridge.bridge_command(aliases=["information"], description = "Displays information about Pingernos")
     async def info(self, ctx):
         embed = Embed()
+        try:
+            stat = await wait_for(Utils.get_server_status("example.aternos.me"), timeout=2)
+        except TimeoutError:
+            latency = "N/A"
+        else:
+            latency = round(stat.latency)
         embed.description = f"""
 {self.bot.user.name} is a bot developed by BlackFur Studios to service Aternos users by displaying server statuses. We are not affiliated with Aternos and are an independent FOSSDEV organization.
 
 **Guilds:** {len(self.bot.guilds)}
 **Users:** {sum(x.member_count for x in self.bot.guilds)}
+**API Latency:** {round(self.bot.latency*1000)}ms
+**Protocol Latency:** {latency}ms
 
 [[Support]](https://discord.gg/Ukr89GrMBk) [[Github]](https://github.com/BlackFurORG/pingernos)
         """
