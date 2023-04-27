31298e68f634cf965a5a80cf1877c9015b5105e9
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2023-04-27 14:13:42 -0400

    pwease work daddy

--- cogs/setserver.py
+++ cogs/setserver.py
@@ -8,8 +8,8 @@ class SetServer(commands.Cog):
     @bridge.bridge_command(aliases=["set"],
                            description="Set the default server to use if no argument is provided in the status command.")
     async def setserver(self, ctx, server=None):
-        if not ctx.author.guild_permissions.manage_guild:
-            return await ctx.respond("You need the `Manage Server` permission to use this command.")
+        #if not ctx.author.guild_permissions.manage_guild:
+        #    return await ctx.respond("You need the `Manage Server` permission to use this command.")
         if server is None:
             return await ctx.respond(
                 "Please provide a server IP to register to this guild. If an IP is already registered, it'll be overwritten")
@@ -27,6 +27,15 @@ class SetServer(commands.Cog):
         cursor.close()
         await ctx.respond(f'The IP has been set to {server}. Use `status` without an argument to view it.')
 
+    @setserver.error
+    async def setserver_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.respond("Please provide a server IP to register to this guild. If an IP is already registered, it'll be overwritten")
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.respond("You need the `Manage Server` permission to use this command.")
+        else:
+            await ctx.respond ("An unknown error occured, this has been logged.")
+            print(error)
 
 def setup(bot):
     bot.add_cog(SetServer(bot))
