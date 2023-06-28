54ed81f79d089e9a096306dc8b5b7faa0f68dbae
Joshua Slui <joshuaslui0203@gmail.com>
2023-06-28 20:40:05 +0200

    Use new utility categorisation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from discord import Intents, Status, Activity, ActivityType
 from discord.ext.bridge import AutoShardedBot
-from utils import Utils
+from utilities.database import mysql_login
+from utilities.data import get_data
 
-data = Utils.get_data()
+data = get_data()
 intents = Intents(guilds=True, guild_messages=True)
 # intents.message_content = True #Uncomment this if you use prefixed command that are not mentions
 bot = AutoShardedBot(intents=intents, command_prefix=data['Prefix'], status=Status.dnd, activity=Activity(type=ActivityType.watching, name="Starting..."))
@@ -13,7 +14,7 @@ BOOTED = False
 @bot.listen()
 async def on_connect():
     print('Connected to Discord!')
-    cursor = await Utils.mysql_login()
+    cursor = await mysql_login()
     database = cursor.cursor()
     database.execute("CREATE TABLE IF NOT EXISTS server (guild_id VARCHAR(255) PRIMARY KEY, server_ip TEXT NOT NULL)")
     database.execute("CREATE TABLE IF NOT EXISTS blacklist (guild_id VARCHAR(21) PRIMARY KEY, reason TEXT NOT NULL)")
