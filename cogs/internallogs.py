1d9cac78d845bde2a9ac62524414d29edef4e409
Joshua Slui <joshuaslui0203@gmail.com>
2023-06-28 20:39:56 +0200

    Use new utility categorisation

--- cogs/internallogs.py
+++ cogs/internallogs.py
@@ -2,7 +2,8 @@ import aiohttp
 from discord.ext import commands
 from discord import Guild, Embed, Webhook
 from discord.ext.bridge import Bot
-from utils import Utils
+from utilities.database import mysql_login
+from utilities.data import Colors, get_data
 
 class InteralLogs(commands.Cog):
     def __init__(self, bot: Bot):
@@ -10,45 +11,45 @@ class InteralLogs(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild: Guild):
-        cursor = await Utils.mysql_login()
+        cursor = await mysql_login()
         database = cursor.cursor()
         database.execute("SELECT * FROM blacklist WHERE guild_id = %s", [guild.id])
 
         try:
             result = database.fetchone()[0]
             print(f'Guild {guild} attempted to add {self.bot.user.name}, but was blacklisted.')
-            embed = Embed(title="Joined a guild!", color=Utils.Colors.red)
+            embed = Embed(title="Joined a guild!", color=Colors.red)
             embed.add_field(name="Name", value=guild.name, inline=True)
             embed.add_field(name="ID", value=guild.id, inline=True)
             embed.description(f'Guild attempted to add {self.bot.user.name}, but is blacklisted:\n**{result["reason"]}**')
             embed.set_thumbnail(url=guild.icon.url)
             async with aiohttp.ClientSession() as client_session:
-                webhook = Webhook.from_url(Utils.get_data()['Logs']["JoinWebhook"], session=client_session)
+                webhook = Webhook.from_url(get_data()['Logs']["JoinWebhook"], session=client_session)
                 await webhook.send(embed=embed, username="Pingernos Logs", avatar_url=self.bot.user.avatar.url)
             await guild.leave()
         except TypeError:
             pass
 
-        embed = Embed(title="Joined a guild!", color=Utils.Colors.green)
+        embed = Embed(title="Joined a guild!", color=Colors.green)
         embed.add_field(name="Name", value=guild.name, inline=True)
         embed.add_field(name="ID", value=guild.id, inline=True)
         embed.add_field(name="Guild member count", value=guild.member_count, inline=True)
         embed.add_field(name="Current server count", value=len(self.bot.guilds), inline=True)
         embed.set_thumbnail(url=guild.icon.url)
         async with aiohttp.ClientSession() as client_session:
-            webhook = Webhook.from_url(Utils.get_data()['Logs']["JoinWebhook"], session=client_session)
+            webhook = Webhook.from_url(get_data()['Logs']["JoinWebhook"], session=client_session)
             await webhook.send(embed=embed, username="Pingernos Logs", avatar_url=self.bot.user.avatar.url)
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild: Guild):
-        embed = Embed(title="Left a guild!", color=Utils.Colors.red)
+        embed = Embed(title="Left a guild!", color=Colors.red)
         embed.add_field(name="Name", value=guild.name, inline=True)
         embed.add_field(name="ID", value=guild.id, inline=True)
         embed.add_field(name="Guild member count", value=guild.member_count, inline=True)
         embed.add_field(name="Current server count", value=len(self.bot.guilds), inline=True)
         embed.set_thumbnail(url=guild.icon.url)
         async with aiohttp.ClientSession() as client_session:
-            webhook = Webhook.from_url(Utils.get_data()['Logs']["LeaveWebhook"], session=client_session)
+            webhook = Webhook.from_url(get_data()['Logs']["LeaveWebhook"], session=client_session)
             await webhook.send(embed=embed, username="Pingernos Logs", avatar_url=self.bot.user.avatar.url)
 
 def setup(bot: Bot):
