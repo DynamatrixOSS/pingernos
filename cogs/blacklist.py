3d1d1452768369e357ccb1968a67bed3c0d386fc
Joshua Slui <joshuaslui0203@gmail.com>
2023-06-28 20:52:16 +0200

    Use modifier function

--- cogs/blacklist.py
+++ cogs/blacklist.py
@@ -2,7 +2,7 @@ import discord
 from discord import slash_command, option
 from discord.ext import commands
 from discord.ext.bridge import Bot
-from utilities.database import modifyData
+from utilities.database import modifier
 
 
 class Blacklist(commands.Cog):
@@ -15,7 +15,7 @@ class Blacklist(commands.Cog):
     @option("reason", str, description="The reason for the blacklist")
     async def blacklist(self, ctx, server, reason):
         """ Blacklist a server from the bot """
-        await modifyData("INSERT IGNORE INTO blacklist (guild_id, reason) VALUES (%s, %s)", [server.id, reason])
+        await modifier("INSERT IGNORE INTO blacklist (guild_id, reason) VALUES (%s, %s)", [server.id, reason])
         guild = self.bot.get_guild(server.id)
         await guild.leave()
         return await ctx.respond(f'Successfully added guild {server} to the blacklist for:\n**{reason}**')
