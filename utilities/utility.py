6d2f826171d1eb5eb0ca2f0b206b38bf3feb9a28
Joshua Slui <joshuaslui0203@gmail.com>
2023-06-29 12:14:42 +0200

    pylint.... i hate you....

--- utilities/utility.py
+++ utilities/utility.py
@@ -1,13 +1,11 @@
 import re
-import discord
 
 
 def check_ip(server: str) -> str:
     if not server.endswith('.aternos.me'):
         server += '.aternos.me'
-    regEx = re.compile(r"\w+\.aternos\.me")
-    matcher = re.findall(regEx, server)
+    regex = re.compile(r"\w+\.aternos\.me")
+    matcher = re.findall(regex, server)
     if not matcher:
         return False
-    else:
-        return matcher[0]
+    return matcher[0]
