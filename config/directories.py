ab5615bdd2e954ef763cb693749b1b474bdbcee3
Joshua Slui <joshuaslui0203@gmail.com>
2024-08-09 01:42:31 +0200

    Add directory for 'cogs'

--- config/directories.py
+++ config/directories.py
@@ -1,6 +1,7 @@
 class Directories:
     migrations = "database/migrations"
     seeders = "database/seeders"
+    modules = "app.commands"  # always use periods instead of slashes as this is how py-cord interprets the file paths for extensions.
 
     def get_directory(self, directory):
         return getattr(self, directory)
