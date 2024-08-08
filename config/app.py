160ef7a0a2ae45a9f7308c7830a3cb9887ad64de
Joshua Slui <joshuaslui0203@gmail.com>
2024-08-09 01:54:28 +0200

    Merge all settings into a central self.settings variable

--- config/app.py
+++ config/app.py
@@ -3,31 +3,32 @@ from config.vault import get_vault
 
 class Settings:
     def __init__(self):
-        self.environment = "development"
-        self.debug = False
-        self.seeding = False
-        self.migrations = True
-        self.manager_guilds = [1256003845188882562]
+        self.settings = {
+            "environment": "development",
+            "debug": False,
+            "seeding": False,
+            "migrations": True,
+            "manager_guilds": [1256003845188882562],
+            "github": "https://github.com/DynamatrixOSS/pingernos",
+            "discord": "https://discord.gg/Z8KGvGWZcY",
+            "privacy": "https://rebel.dynamatrix.com/privacy",
+            "invite": "https://rebel.dynamatrix.com/invite",
+        }
 
-        self.github = "https://github.com/DynamatrixOSS/pingernos"
-        self.discord = "https://discord.gg/Z8KGvGWZcY"
-        self.privacy = "https://rebel.dynamatrix.com/privacy"
-        self.invite = "https://rebel.dynamatrix.com/invite"
+    def get_setting(self, setting: str):
+        return self.settings.get(setting, None)
 
-    def get_setting(self, setting):
-        return getattr(self, setting)
+    def set_setting(self, setting: str, value) -> None:
+        self.settings[setting] = value
 
-    def get_settings(self):
-        return self
-
-    def get_environment(self):
-        return self.environment
+    def get_environment(self) -> str:
+        return self.settings['environment']
 
     def set_environment(self, environment: str):
         allowed_environments = ["prod", "production", "development", "dev", "testing"]
         environment = environment.lower()
         if any(allowed_environment in environment for allowed_environment in allowed_environments):
-            self.environment = environment
+            self.settings["environment"] = environment
             return {'message': 'Environment successfully set to {}'.format(environment), 'code': 200}
 
         raise ValueError('Environment {} is not valid. Must be one of following values: {}'.format(environment, allowed_environments))
