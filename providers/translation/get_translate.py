d40bbc00d07f9a2d242ab1cfdbb6f96942997e49
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-08-08 20:07:32 -0400

    ok

--- providers/translation/get_translate.py
+++ providers/translation/get_translate.py
@@ -1,6 +1,7 @@
 from os import walk
 from json import load
 from sys import exit as sysexit
+
 class Translation:
     def _get_translation_map(self) -> dict:
         "You shouldn't have to use this unless your debugging."
@@ -22,19 +23,24 @@ class Translation:
                 return translation_map["en-US"][message_code] # Default to US
             except KeyError:
                 return "No message available for this given message code"
+
 if __name__ == "__main__":
     #Just some tests
     translation_class = Translation()
     if translation_class.get_translation("en-GB", "test") != "British!":
         print ("Test 1 failed")
         sysexit(1)
+
     if translation_class.get_translation("thislanguage-doesnotexist", "test") != "Works!":
         print ("Test 2 failed")
         sysexit(1)
+
     if translation_class.get_translation("en-US", "test") != "Works!":
         print ("Test 3 failed")
         sysexit(1)
+
     if translation_class.get_translation("en-US", "thisdoesnotexist") != "No message available for this given message code":
         print ("Test 4 failed")
         sysexit(1)
+
     print ("All tests passed!")
