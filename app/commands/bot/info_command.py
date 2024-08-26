7f218d9b8c4a8feb4faca36e7bd3b5761bcdaa2b
Joshua Slui <joshuaslui0203@gmail.com>
2024-08-26 13:23:27 +0200

    change order of function parameters

--- app/commands/bot/info_command.py
+++ app/commands/bot/info_command.py
@@ -15,25 +15,25 @@ class Source(View):  # Create a class that subclasses discord.ui.View
     async def add_buttons(self):
         # Add a URL button to the view
         self.add_item(Button(
-            label=await t(self.ctx, 'Source'),
+            label=await t('Source', self.ctx),
             style=ButtonStyle.link,
             emoji="<github:1271230731691229265>",
             url=self.settings.get_setting('github')
         ))
         self.add_item(Button(
-            label=await t(self.ctx, 'Support'),
+            label=await t('Support', self.ctx),
             style=ButtonStyle.link,
             emoji="<:discord:1271235054697189376>",
             url=self.settings.get_setting('discord')
         ))
         self.add_item(Button(
-            label=await t(self.ctx, 'Privacy'),
+            label=await t('Privacy', self.ctx),
             style=ButtonStyle.link,
             emoji="<:data:1271237825467121796>",
             url=self.settings.get_setting('privacy')
         ))
         self.add_item(Button(
-            label=await t(self.ctx, 'Invite'),
+            label=await t('Invite', self.ctx),
             style=ButtonStyle.link,
             emoji="<:invite:1271238312077561917>",
             url=self.settings.get_setting('invite')
@@ -58,14 +58,14 @@ class BotInfo(commands.Cog):
         information_embed = Embed()
         information_embed.colour = Color.purple()
         information_embed.set_author(
-            name=await t(ctx, 'Dynamatrix OSS by Dynamatrix'),
+            name=await t('Dynamatrix OSS by Dynamatrix', ctx),
             icon_url=self.bot.user.avatar.url
         )
-        information_embed.set_footer(text=await t(ctx, 'Response time', [round(self.bot.latency * 1000)]))
+        information_embed.set_footer(text=await t('Response time', ctx, [round(self.bot.latency * 1000)]))
         information_embed.description = await t(
-            ctx,
             'is a bot developed by Dynamatrix OSS',
-            [self.bot.user.mention, self.bot.user.display_name]
+            ctx,
+            parameters=[self.bot.user.mention, self.bot.user.display_name]
         )
 
         await ctx.respond(embed=information_embed, view=await source.add_buttons(), ephemeral=True)
